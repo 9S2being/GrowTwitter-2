a32bf5ff6697369aec37849da8bb773a057e358e
João Gabriel <karlinski2005@gmail.com>
2024-02-29 22:09:46 -0300

    - Folders of the project created - configuration completed - database connected with the project - tables: user, tweet, like, follow, and reply created

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("teste")
+import express from "express";
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(3333, () => {
+    console.log("Server running on port 3333.");
+});
